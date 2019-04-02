18d8769ff89b8dcb6b9aeff328e523d4829625fe
Andy Ly <andywindsong@gmail.com>
2019-04-02 16:33:25 -0500

    login seems to work

--- MSALAngularDemoApp/src/app/app.module.ts
+++ MSALAngularDemoApp/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component'
-import {ProductComponent} from './product/product.component'
-import {ErrorComponent} from './error.component'
-import {ProductDetailComponent} from './product/product-detail.component'
-import {ProductService} from './product/product.service';
-import {appRoutes} from './app.routes';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {MsalModule} from "@azure/msal-angular";
-import { MsalInterceptor} from "@azure/msal-angular";
-import {LogLevel} from "msal";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
+import { ProductComponent } from './product/product.component'
+import { ErrorComponent } from './error.component'
+import { ProductDetailComponent } from './product/product-detail.component'
+import { ProductService } from './product/product.service';
+import { appRoutes } from './app.routes';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { MsalModule } from "@azure/msal-angular";
+import { MsalInterceptor } from "@azure/msal-angular";
+import { LogLevel } from "msal";
 import { TodoListComponent } from './todo-list/todo-list.component';
-import {TodoListService} from "./todo-list/todo-list.service";
-import {UserDataComponent} from "./user-data/user-data.component";
+import { TodoListService } from "./todo-list/todo-list.service";
+import { UserDataComponent } from "./user-data/user-data.component";
 import { HttpServiceHelper } from './common/HttpServiceHelper';
 
 export function loggerCallback(logLevel, message, piiEnabled) {
@@ -23,7 +23,7 @@ export function loggerCallback(logLevel, message, piiEnabled) {
 }
 
 
-export const protectedResourceMap:[string, string[]][]=[ ['https://buildtodoservice.azurewebsites.net/api/todolist',['api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user']] , ['https://graph.microsoft.com/v1.0/me', ['user.read']] ];
+export const protectedResourceMap: [string, string[]][] = [['https://buildtodoservice.azurewebsites.net/api/todolist', ['api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user']], ['https://graph.microsoft.com/v1.0/me', ['user.read']]];
 
 
 @NgModule({
@@ -34,28 +34,28 @@ export const protectedResourceMap:[string, string[]][]=[ ['https://buildtodoserv
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes,{useHash:true}) ,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     MsalModule.forRoot({
-        clientID: '6226576d-37e9-49eb-b201-ec1eeb0029b6',
-        authority: "https://login.microsoftonline.com/common/",
-        validateAuthority: true,
-        redirectUri: "http://localhost:4200/",
-        cacheLocation : "localStorage",
-        postLogoutRedirectUri: "http://localhost:4200/",
-        navigateToLoginRequestUrl: true,
-        popUp: false,
-        consentScopes: [ "user.read", "api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user"],
-        unprotectedResources: ["https://www.microsoft.com/en-us/"],
-        protectedResourceMap: protectedResourceMap,
-        logger: loggerCallback,
-        correlationId: '1234',
-        level: LogLevel.Info,
-        piiLoggingEnabled: true
-      }
+      clientID: '3444cbd3-f727-47a3-8fa5-22a63a87a899',
+      authority: "https://login.microsoftonline.com/Project3Authorization.onmicrosoft.com",
+      validateAuthority: true,
+      redirectUri: "http://localhost:4200/",
+      cacheLocation: "localStorage",
+      postLogoutRedirectUri: "http://localhost:4200/",
+      navigateToLoginRequestUrl: true,
+      popUp: false,
+      // consentScopes: ["user.read", "https://google.com"],
+      // unprotectedResources: ["https://www.microsoft.com/en-us/"],
+      protectedResourceMap: protectedResourceMap,
+      logger: loggerCallback,
+      correlationId: '1234',
+      level: LogLevel.Info,
+      piiLoggingEnabled: true
+    }
     ),
   ],
   providers: [ProductService, TodoListService, HttpServiceHelper,
-     {provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true}
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
